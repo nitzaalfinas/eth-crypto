2b64c95d21a98271a33ac19b302b2ab5a2c2ec66
Nitza Alfinas <nitzaalfinas@gmail.com>
2022-10-25 08:35:44 +0700

    add buffer

--- src/create-identity.js
+++ src/create-identity.js
@@ -1,6 +1,8 @@
 import { utils as ethersUtils, Wallet } from 'ethers';
 import { stripHexPrefix } from 'ethereumjs-util';
 
+var Buffer = require('buffer/').Buffer
+
 const MIN_ENTROPY_SIZE = 128;
 const { keccak256 } = ethersUtils;
 
